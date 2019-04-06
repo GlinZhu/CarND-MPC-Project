b48d5f0f2ee552ae8da61cf334fd446d6cab4a6c
Guilin Zhu <guilinzhu@Guilins-MacBook-Pro.local>
2019-04-06 17:22:48 -0400

    Update to MPC.cpp

--- src/helpers.h
+++ src/helpers.h
@@ -11,15 +11,15 @@ using std::string;
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
 string hasData(string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.rfind("}]");
-  if (found_null != string::npos) {
+    auto found_null = s.find("null");
+    auto b1 = s.find_first_of("[");
+    auto b2 = s.rfind("}]");
+    if (found_null != string::npos) {
+        return "";
+    } else if (b1 != string::npos && b2 != string::npos) {
+        return s.substr(b1, b2 - b1 + 2);
+    }
     return "";
-  } else if (b1 != string::npos && b2 != string::npos) {
-    return s.substr(b1, b2 - b1 + 2);
-  }
-  return "";
 }
 
 //
@@ -28,36 +28,45 @@ string hasData(string s) {
 
 // Evaluate a polynomial.
 double polyeval(const VectorXd &coeffs, double x) {
-  double result = 0.0;
-  for (int i = 0; i < coeffs.size(); ++i) {
-    result += coeffs[i] * pow(x, i);
-  }
-  return result;
+    double result = 0.0;
+    for (int i = 0; i < coeffs.size(); ++i) {
+        result += coeffs[i] * pow(x, i);
+    }
+    return result;
+}
+
+// take the derivative of polynomial fucntion
+double polyderi(const VectorXd &coeffs, double x) {
+    double result = 0.0;
+    for (int i = 1; i < coeffs.size(); ++i) {
+        result += i*coeffs[i] * pow(x, i-1);
+    }
+    return result;
 }
 
 // Fit a polynomial.
 // Adapted from:
 // https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
 VectorXd polyfit(const VectorXd &xvals, const VectorXd &yvals, int order) {
-  assert(xvals.size() == yvals.size());
-  assert(order >= 1 && order <= xvals.size() - 1);
-
-  Eigen::MatrixXd A(xvals.size(), order + 1);
-
-  for (int i = 0; i < xvals.size(); ++i) {
-    A(i, 0) = 1.0;
-  }
-
-  for (int j = 0; j < xvals.size(); ++j) {
-    for (int i = 0; i < order; ++i) {
-      A(j, i + 1) = A(j, i) * xvals(j);
+    assert(xvals.size() == yvals.size());
+    assert(order >= 1 && order <= xvals.size() - 1);
+    
+    Eigen::MatrixXd A(xvals.size(), order + 1);
+    
+    for (int i = 0; i < xvals.size(); ++i) {
+        A(i, 0) = 1.0;
     }
-  }
-
-  auto Q = A.householderQr();
-  auto result = Q.solve(yvals);
-
-  return result;
+    
+    for (int j = 0; j < xvals.size(); ++j) {
+        for (int i = 0; i < order; ++i) {
+            A(j, i + 1) = A(j, i) * xvals(j);
+        }
+    }
+    
+    auto Q = A.householderQr();
+    auto result = Q.solve(yvals);
+    
+    return result;
 }
 
 #endif  // HELPERS_H
